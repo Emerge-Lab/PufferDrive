f1f38cedd9cb7276dcf121a21af8ad435395bd62
Daphne Cornelisse <cor.daphne@gmail.com>
2025-09-17 11:54:18 -0400

    Fix typo

--- pufferlib/ocean/drive/drive.py
+++ pufferlib/ocean/drive/drive.py
@@ -36,7 +36,7 @@ class Drive(pufferlib.PufferEnv):
         self.reward_offroad_collision = reward_offroad_collision
         self.reward_goal_post_respawn = reward_goal_post_respawn
         self.reward_vehicle_collision_post_respawn = reward_vehicle_collision_post_respawn
-        self.reward_ade = (reward_ade,)
+        self.reward_ade = reward_ade
         self.spawn_immunity_timer = spawn_immunity_timer
         self.human_agent_idx = human_agent_idx
         self.resample_frequency = resample_frequency
@@ -137,6 +137,7 @@ class Drive(pufferlib.PufferEnv):
                         reward_offroad_collision=self.reward_offroad_collision,
                         reward_goal_post_respawn=self.reward_goal_post_respawn,
                         reward_vehicle_collision_post_respawn=self.reward_vehicle_collision_post_respawn,
+                        reward_ade=self.reward_ade,
                         spawn_immunity_timer=self.spawn_immunity_timer,
                         map_id=map_ids[i],
                         max_agents=nxt - cur,
