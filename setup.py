e435443bcdddaad042c398e6c58b1fad19751024
AJE <231052006+aje-valeo@users.noreply.github.com>
2025-09-19 11:34:51 +0200

    Add parser support of in-line comments
    - Add also support to one line comment with #
    - Revert the drive.ini file changes

--- setup.py
+++ setup.py
@@ -253,6 +253,12 @@ if not NO_OCEAN:
     for c_ext in c_extensions:
         if "drive" in c_ext.name:
             c_ext.sources.append("inih-r62/ini.c")
+            c_ext.extra_compile_args.extend(
+                [
+                    '-DINI_START_COMMENT_PREFIXES="#"',
+                    '-DINI_INLINE_COMMENT_PREFIXES="#"',
+                ]
+            )
 
         if "impulse_wars" in c_ext.name:
             print(f"Adding {c_ext.name} to extra objects")
