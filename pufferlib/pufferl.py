00d44cac2a3c4fc1cfb4a3d5fe3d10cfa7ac546d
Aditya Gupta <adigupta2602@gmail.com>
2026-03-09 19:30:41 -0400

    Reinstated reward clamping

--- pufferlib/pufferl.py
+++ pufferlib/pufferl.py
@@ -295,8 +295,7 @@ class PuffeRL:
 
                 logits, value = self.policy.forward_eval(o_device, state)
                 action, logprob, _ = pufferlib.pytorch.sample_logits(logits)
-                # Removed reward clamping
-                # r = torch.clamp(r, -1, 1)
+                r = torch.clamp(r, -1, 1)
 
             profile("eval_copy", epoch)
             with torch.no_grad():
