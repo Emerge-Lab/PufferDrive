042778d69d92f784a0f80948bf0b884b6466e54b
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-06-18 20:58:54 +0200

    build puffer

---
+++ scripts/0_build_puffer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=train_puffer
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=16
+#SBATCH --output=/lustre/scwpod02/client/kyutai/kesai/bernhard/carla_closed_loop/results/logs/eval_server_%a_%A.out
+#SBATCH --error=/lustre/scwpod02/client/kyutai/kesai/bernhard/carla_closed_loop/results/logs/eval_server_%a_%A.err
+#SBATCH --partition=kyutai
+
+# print info about current job
+echo "START TIME: $(date)"
+start=`date +%s`
+
+source .venv/bin/activate
+python setup.py build_ext --inplace --force
+
+end=`date +%s`
+runtime=$((end-start))
+echo "END TIME: $(date)"
+echo "Runtime: ${runtime}"
