d117988e273c4a9953e55252a2b309d8b70d45f5
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-06-18 20:19:47 +0200

    Add simple slurm script to train puffer model

---
+++ scripts/train_model_slurm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=train_puffer
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --output=/lustre/scwpod02/client/kyutai/kesai/bernhard/carla_closed_loop/results/logs/eval_server_%a_%A.out
+#SBATCH --error=/lustre/scwpod02/client/kyutai/kesai/bernhard/carla_closed_loop/results/logs/eval_server_%a_%A.err
+#SBATCH --partition=kyutai
+
+# print info about current job
+echo "START TIME: $(date)"
+start=`date +%s`
+
+.venv/bin/puffer train puffer_drive --wandb --vec.num-envs 16
+
+end=`date +%s`
+runtime=$((end-start))
+echo "END TIME: $(date)"
+echo "Runtime: ${runtime}"
