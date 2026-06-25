8ae782815fbc344bc97aa511d2de75623da94c07
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-06-25 13:58:01 +0200

    more cpus for compile

--- scripts/0_build_puffer.sh
+++ scripts/0_build_puffer.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=1-00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=24
 #SBATCH --output=/lustre/scwpod02/client/kyutai/kesai/bernhard/carla_closed_loop/results/logs/eval_server_%a_%A.out
 #SBATCH --error=/lustre/scwpod02/client/kyutai/kesai/bernhard/carla_closed_loop/results/logs/eval_server_%a_%A.err
 #SBATCH --partition=kyutai
