d950ad51a5c2827593c4ce7fecaa2103c421b24f
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-08-06 17:06:45 +0200

    reduce envs to 16

--- scripts/3_eval_model_slurm.sh
+++ scripts/3_eval_model_slurm.sh
@@ -6,25 +6,27 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=125G
 #SBATCH --cpus-per-task=18
-#SBATCH --output=/home/bjaeger/PufferDrive/experiments/k_scaled_0006/puffer_drive_nht7wfsc/eval/eval_nuPlan_%a_%A.out
-#SBATCH --error=/home/bjaeger/PufferDrive/experiments/k_scaled_0006/puffer_drive_nht7wfsc/eval/eval_nuPlan_%a_%A.err
+#SBATCH --output=/home/bjaeger/PufferDrive/experiments/k_scaled_0006/puffer_drive_nht7wfsc/eval2/eval_nuPlan_%a_%A.out
+#SBATCH --error=/home/bjaeger/PufferDrive/experiments/k_scaled_0006/puffer_drive_nht7wfsc/eval2/eval_nuPlan_%a_%A.err
 #SBATCH --partition=dev
 
 # print info about current job
 echo "START TIME: $(date)"
 start=`date +%s`
 
-RESULTS_PATH=/home/bjaeger/PufferDrive/experiments/k_scaled_0006/puffer_drive_nht7wfsc/eval
+RESULTS_PATH=/home/bjaeger/PufferDrive/experiments/k_scaled_0006/puffer_drive_nht7wfsc/eval2
 MODEL_PATH=/home/bjaeger/PufferDrive/experiments/k_scaled_0006/puffer_drive_nht7wfsc/models/model_puffer_drive_007451.pt
 
 source .venv/bin/activate
 .venv/bin/puffer eval puffer_drive carla \
+    vec.num_envs=16 \
     eval.action_selection=mean \
     eval.output_name=${RUN_NAME} \
     load_model_path=${MODEL_PATH}
 
 .venv/bin/puffer eval puffer_drive nuplan_single \
     env.map_dir=/home/shared/data/nuPlan/PufferDrive \
+    vec.num_envs=16 \
     eval.action_selection=mean \
     eval.output_name=${RUN_NAME} \
     load_model_path=${MODEL_PATH}
