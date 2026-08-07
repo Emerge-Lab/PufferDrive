73421a0f7de1c3d9082a0f374c6f9abc4db73604
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-08-07 14:58:52 +0200

    run eval script

--- scripts/3_eval_model_slurm.sh
+++ scripts/3_eval_model_slurm.sh
@@ -6,16 +6,16 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=125G
 #SBATCH --cpus-per-task=18
-#SBATCH --output=/home/bjaeger/PufferDrive/experiments/k_exp_0000/puffer_drive_8um75rpq/eval/eval_nuPlan_%a_%A.out
-#SBATCH --error=/home/bjaeger/PufferDrive/experiments/k_exp_0000/puffer_drive_8um75rpq/eval/eval_nuPlan_%a_%A.err
+#SBATCH --output=/home/bjaeger/PufferDrive/experiments/logs/eval_%a_%A.out
+#SBATCH --error=/home/bjaeger/PufferDrive/experiments/logs/eval_%a_%A.err
 #SBATCH --partition=dev
 
 # print info about current job
 echo "START TIME: $(date)"
 start=`date +%s`
 
-export RUN_NAME=k_exp_0000
-MODEL_PATH=/home/bjaeger/PufferDrive/experiments/k_exp_0000/puffer_drive_8um75rpq/best_models/best_trainer_state_020950.pt
+export RUN_NAME=k_fast_0001_1000
+MODEL_PATH=/home/bjaeger/PufferDrive/experiments/k_fast_0001_1000/final_model.pt
 
 source .venv/bin/activate
 .venv/bin/puffer eval puffer_drive carla \
