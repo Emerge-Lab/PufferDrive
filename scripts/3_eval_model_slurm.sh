9efb3dc0fe5ed22d1413ca184176b847e5535e45
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-08-10 12:12:02 +0200

    eval k_exp_0001_3000

--- scripts/3_eval_model_slurm.sh
+++ scripts/3_eval_model_slurm.sh
@@ -14,8 +14,8 @@
 echo "START TIME: $(date)"
 start=`date +%s`
 
-export RUN_NAME=k_exp_0001_2000
-MODEL_PATH=/home/bjaeger/PufferDrive/experiments/k_exp_0001/puffer_drive_pnlhobgh/puffer_drive_pnlhobgh.pt
+export RUN_NAME=k_exp_0001_3000
+MODEL_PATH=/home/bjaeger/PufferDrive/experiments/k_exp_0001/puffer_drive_m10p3mck/puffer_drive_m10p3mck.pt
 #final_model.pt
 source .venv/bin/activate
 python setup.py build_ext --inplace --force
