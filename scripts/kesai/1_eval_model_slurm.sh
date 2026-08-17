e37c60e81e9765e8482124e9b5ef5c4c956bb2d4
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-08-17 16:49:31 +0200

    eval without margin eval_perceived_size_margin_m

--- scripts/kesai/1_eval_model_slurm.sh
+++ scripts/kesai/1_eval_model_slurm.sh
@@ -3,32 +3,36 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --time=1-00:00
-#SBATCH --gres=gpu:1
-#SBATCH --mem=125G
-#SBATCH --cpus-per-task=18
+#SBATCH --gres=gpu:8
+#SBATCH --mem=1007G
+#SBATCH --cpus-per-task=144
 #SBATCH --output=/home/bjaeger/PufferDrive/experiments/logs/eval_%a_%A.out
 #SBATCH --error=/home/bjaeger/PufferDrive/experiments/logs/eval_%a_%A.err
 #SBATCH --partition=dev
-#SBATCH --array=1-8
 
 # print info about current job
 echo "START TIME: $(date)"
 start=`date +%s`
 
-export RUN_NAME=k_nightly_2026-08-13_3000
+export RUN_NAME=k_scaled_0008_1000
 MODEL_PATH=/home/bjaeger/PufferDrive/experiments/${RUN_NAME}/final_model.pt
 
 source .venv/bin/activate
 bash scripts/kesai/build_ext_if_changed.sh /home/bjaeger/PufferDrive || exit 1
 
 .venv/bin/puffer eval puffer_drive carla \
-    vec.num_envs=16 \
+    vec.num_envs=128 \
+    env.eval_use_carla_vehicle_dims=False \
+    env.eval_perceived_size_margin_m=0.0 \
     eval.output_name=${RUN_NAME} \
     load_model_path=${MODEL_PATH} \
     wandb=True
 
 .venv/bin/puffer eval puffer_drive nuplan_single \
     env.map_dir=/home/shared/data/nuPlan/PufferDrive \
+    vec.num_envs=128 \
+    eval.max_sdc_replay_workers=128 \
+    env.eval_perceived_size_margin_m=0.0 \
     eval.output_name=${RUN_NAME} \
     load_model_path=${MODEL_PATH} \
     wandb=True
