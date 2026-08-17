e14b08f02295dfc4e165ef46ddafbfbe17a6ccb5
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-08-17 18:01:49 +0200

    eval again

--- scripts/kesai/1_eval_model_slurm.sh
+++ scripts/kesai/1_eval_model_slurm.sh
@@ -22,8 +22,8 @@ bash scripts/kesai/build_ext_if_changed.sh /home/bjaeger/PufferDrive || exit 1
 
 .venv/bin/puffer eval puffer_drive carla \
     vec.num_envs=64 \
-    env.eval_use_carla_vehicle_dims=False \
-    env.eval_perceived_size_margin_m=0.1 \
+    env.eval_use_carla_vehicle_dims=True \
+    env.eval_perceived_size_margin_m=0.0 \
     eval.output_name=${RUN_NAME} \
     load_model_path=${MODEL_PATH} \
     wandb=True
@@ -32,7 +32,7 @@ bash scripts/kesai/build_ext_if_changed.sh /home/bjaeger/PufferDrive || exit 1
     env.map_dir=/home/shared/data/nuPlan/PufferDrive \
     vec.num_envs=64 \
     eval.max_sdc_replay_workers=64 \
-    env.eval_perceived_size_margin_m=0.1 \
+    env.eval_perceived_size_margin_m=0.0 \
     eval.output_name=${RUN_NAME} \
     load_model_path=${MODEL_PATH} \
     wandb=True
