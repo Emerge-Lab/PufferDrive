ddd128ff4ae3160c5b83c9c0fa8064bd7a7a4a3b
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-08-27 20:42:29 +0200

    eval sample

--- scripts/kesai/7_eval_8node_slurm.sh
+++ scripts/kesai/7_eval_8node_slurm.sh
@@ -57,8 +57,8 @@ fi
     eval.goal_speed=3.0 \
     env.max_speed_mps=13.33 \
     env.disable_red_light_infractions=0 \
-    eval.action_selection=mode \
-    eval.output_name=${RUN_NAME}_medium2 \
+    eval.action_selection=sample \
+    eval.output_name=${RUN_NAME}_medium3 \
     load_model_path=${MODEL_PATH} \
     wandb=True
 #     eval.obs_slots_partners_n=40 \
