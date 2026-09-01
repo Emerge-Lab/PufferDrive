a7d15bc0527b095adc74ce50045cb3f557eaf864
Bernhard Jaeger <B.Jaeger1@gmx.net>
2026-09-01 14:55:48 +0200

    eval nuPlan again with proper despawn

--- scripts/kesai/7_eval_8node_slurm.sh
+++ scripts/kesai/7_eval_8node_slurm.sh
@@ -13,7 +13,7 @@
 echo "START TIME: $(date)"
 start=$(date +%s)
 
-export RUN_NAME=k_scaled_0034_1000
+export RUN_NAME=k_scaled_0035_1000
 echo ${RUN_NAME}
 
 export MODEL_PATH=/home/bjaeger/PufferDrive/experiments/${RUN_NAME}/final_model.pt
@@ -36,24 +36,24 @@ fi
 
 # parallel_eval places one shard per allocated node via srun, so each shard's 64
 # env workers get a full node's cores instead of sharing the batch host.
-.venv/bin/python scripts/parallel_eval.py carla \
-    --total-scenarios 40000 \
-    --num-nodes 8 \
-    env.map_dir=/home/bjaeger/PufferDrive/pufferlib/resources/drive/binaries/carla \
-    vec.num_envs=64 \
-    eval.render_filter=all_infractions \
-    eval.capture_observations=true \
-    eval.reward_comfort=0.0 \
-    eval.reward_lane_center=0.0075 \
-    env.eval_perceived_size_margin_m=0.2 \
-    eval.min_goal_spacing=20 \
-    eval.max_goal_spacing=200 \
-    env.disable_red_light_infractions=1 \
-    env.traffic_light_junction_phases=0 \
-    env.eval_standstill_jerk_deadband_mps3=1.5 \
-    eval.output_name=${RUN_NAME}_fast \
-    load_model_path=${MODEL_PATH} \
-    wandb=True
+#.venv/bin/python scripts/parallel_eval.py carla \
+#    --total-scenarios 40000 \
+#    --num-nodes 8 \
+#    env.map_dir=/home/bjaeger/PufferDrive/pufferlib/resources/drive/binaries/carla \
+#    vec.num_envs=64 \
+#    eval.render_filter=all_infractions \
+#    eval.capture_observations=true \
+#    eval.reward_comfort=0.0 \
+#    eval.reward_lane_center=0.0075 \
+#    env.eval_perceived_size_margin_m=0.2 \
+#    eval.min_goal_spacing=20 \
+#    eval.max_goal_spacing=200 \
+#    env.disable_red_light_infractions=1 \
+#    env.traffic_light_junction_phases=0 \
+#    env.eval_standstill_jerk_deadband_mps3=1.5 \
+#    eval.output_name=${RUN_NAME}_fast \
+#    load_model_path=${MODEL_PATH} \
+#    wandb=True
 #     eval.obs_slots_partners_n=40 \
 #     eval.goal_regen_mode=rolling \
 #     env.max_speed_mps=13.33 \
@@ -70,7 +70,7 @@ python -m pufferlib.pufferl eval puffer_drive nuplan_multi \
     eval.disable_red_light_infractions=1 \
     eval.render_filter=all_infractions \
     eval.capture_observations=true \
-    eval.output_name=${RUN_NAME}_fast \
+    eval.output_name=${RUN_NAME}_fast2 \
     load_model_path=${MODEL_PATH} \
     wandb=True
 
